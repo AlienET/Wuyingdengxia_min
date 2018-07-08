94bd564b35879a7769f5fc81da4d44de2142e632
AlienET <wzj_et@sina.com>
2018-07-08 20:11:08 +0800

    et

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ App({
     return result;
   },
   // 接口
-  InterfaceUrl: 'https://cloud.yszg.org/index.php/API/',
+  InterfaceUrl: 'https://yszg.org/index.php/API/',
   // 已授权
   authSetting: false,
   // openid
