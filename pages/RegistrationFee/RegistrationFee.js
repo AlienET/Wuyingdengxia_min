d44841bc6383494c7e0a03575720bd0a4e7bb8b5
AlienET <wzj_et@sina.com>
2019-02-25 16:47:23 +0800

    et

--- pages/RegistrationFee/RegistrationFee.js
+++ pages/RegistrationFee/RegistrationFee.js
@@ -11,11 +11,11 @@ Page({
     //发票信息
     InvoiceInformation: '',
     //地址
-    MailingAddress:'',
+    MailingAddress: '',
     //会名
-    meet_title:'',
+    meet_title: '',
     //费用
-    meet_regist_fee:''
+    meet_regist_fee: ''
   },
   invoiceTap: function() {
     var that = this;
@@ -23,11 +23,145 @@ Page({
       url: '../invoice/invoice',
     })
   },
-  addressTap:function(){
+  addressTap: function() {
     var that = this;
     wx.navigateTo({
       url: '../addressDZ/addressDZ',
     })
+  },
+  //报名
+  OnSignUpTap: function(res) {
+    var that = this;
+    if (that.data.InvoiceInformation && that.data.MailingAddress) {
+
+      var data = new Object();
+      var meet_regist_fee = that.data.meet_regist_fee * 100;
+      data.appid = "wx8dbc9156e40232a7";
+      data.spbill_create_ip = "wydx";
+      data.order_type = "0";
+      data.toId = app.userData.userid;
+      data.userid = app.userData.userid;
+      data.trade_type = "JSAPI";
+      data.js_code = app.wx_code;
+      data.openid = app.mini_openid;
+      data.total_fee = meet_regist_fee.toString();
+      data = JSON.stringify(data); // 转JSON字符串
+      var data = RSA.sign(data);
+      wx.request({
+        url: 'http://39.106.49.2:8081/usermanage/UnifiedOrder',
+        data: {
+          data: data
+        },
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: function(res) {
+          console.log(res)
+          console.log(res.data.data)
+        }
+      })
+
+    } else if (that.data.InvoiceInformation == '' && that.data.MailingAddress == '') {
+
+      var data = new Object();
+      var meet_regist_fee = that.data.meet_regist_fee * 100;
+      data.appid = "wx8dbc9156e40232a7";
+      data.spbill_create_ip = "wydx";
+      data.order_type = "0";
+      data.toId = app.userData.userid;
+      data.userid = app.userData.userid;
+      data.trade_type = "JSAPI";
+      data.js_code = app.wx_code;
+      data.openid = app.mini_openid;
+      data.total_fee = meet_regist_fee.toString();
+      data = JSON.stringify(data); // 转JSON字符串
+      var data = RSA.sign(data);
+      wx.request({
+        url: 'http://39.106.49.2:8081/usermanage/UnifiedOrder',
+        data: {
+          data: data
+        },
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: function(res) {
+          console.log(res)
+          console.log(res.data.data)
+          wx.requestPayment({
+            'timeStamp': res.data.data.timeStamp,
+            'nonceStr': res.data.data.nonceStr,
+            'package': res.data.data.package,
+            'signType': 'MD5',
+            'paySign': res.data.data.sign,
+            'success': function(res) {
+              console.log(res)
+              var reservationInformationData = app.reservationInformationData;
+              reservationInformationData.mail_id = '';
+              reservationInformationData.receipt_id = '';
+              var objs = JSON.stringify(reservationInformationData); // 转JSON字符串
+              var objss = RSA.sign(objs);
+              wx.showLoading({
+                title: '提交中',
+                success: function(res) {
+                  wx.request({
+                    // url: app.InterfaceUrl+'activitymanage/AttendMeet',
+                    url: 'http://39.106.49.2:8083/activitymanage/AttendMeet',
+                    data: {
+                      data: data
+                    },
+                    header: {
+                      'content-type': 'application/x-www-form-urlencoded'
+                    },
+                    method: 'POST',
+                    success: function(res) {
+                      console.log(res);
+                      if (res.data.code == 1) {
+                        wx.navigateTo({
+                          url: '../applySubmit/applySubmit?title=' + reservationInformationData.meet_title + '&begin_time=' + reservationInformationData.begin_time + '&end_time=' + reservationInformationData.end_time,
+                        })
+                      } else {
+                        wx.hideLoading()
+                        wx.showToast({
+                          title: '提交失败...',
+                          icon: 'none',
+                          duration: 2000
+                        })
+                      }
+                    },
+                    fail: function(error) {
+                      console.log(error)
+                    }
+                  })
+                }
+              })
+            },
+            'fail': function(res) {
+              console.log(res)
+              wx.showToast({
+                title: '报名支付失败',
+                icon: 'success',
+                duration: 2000
+              });
+            },
+            'complete': function(res) {
+              console.log(res)
+            }
+          })
+        }
+      })
+
+      console.log('也可报名')
+    } else {
+      console.log(that.data.MailingAddress)
+      console.log(that.data.InvoiceInformation)
+      wx.showToast({
+        title: '请完善信息',
+        icon: 'none',
+        duration: 1500
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面加载
